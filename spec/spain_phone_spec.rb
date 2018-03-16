e7b5eb14f7b393461aa9efee66f60c376af5e9ee
nevadajames <nevadajames@gmail.com>
2018-03-16 13:27:59 +0100

    more tests

--- spec/spain_phone_spec.rb
+++ spec/spain_phone_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe SpainPhone do
 
   before(:all) do
-    @phone = SpainPhone::Phone.new('634213351')
+    @landline = SpainPhone::Phone.new('937886215')
+    @mobile_phone = SpainPhone::Phone.new('634213351')
+    @invalid_phone_numbers = ['0123','345345','999333666555', '222555123']
   end
 
 
@@ -10,14 +12,35 @@ RSpec.describe SpainPhone do
   end
 
   it "returns true for valid phone" do
-    expect(@phone.valid?).to be true
+    [@landline, @mobile_phone].each do |phone|
+      expect(phone.valid?).to be true
+    end
   end
 
   it "returns mobile for valid mobile phone" do
-    expect(@phone.phone_type).to eq('mobile')
+    expect(@mobile_phone.phone_type).to eq('mobile')
   end
 
   it "returns international number" do
-    expect(@phone.international).to eq(['+34', @phone.phone_number].join)
+    [@landline, @mobile_phone].each do |phone|
+      expect(phone.international).to eq(['+34', phone.phone_number].join)
+    end
+  end
+
+  it "returns false for invalid numbers" do |invalid_number|
+    @invalid_phone_numbers.each do
+      invalid = SpainPhone::Phone.new(invalid_number)
+      expect(invalid.valid?).to be false
+    end
+  end
+
+  it "only returns area code for valid numbers" do
+    @invalid_phone_numbers.each do |invalid_number|
+      invalid = SpainPhone::Phone.new(invalid_number)
+      expect(invalid.area_code).to be nil
+    end
+    [@landline, @mobile_phone].each do |phone|
+      expect(phone.area_code).to eq(phone.phone_number[0..2])
+    end
   end
 end
